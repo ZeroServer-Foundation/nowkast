f3210845fe5c7c08023d1cedcddfe359b88f9f65
Wally Trenholm <wally416@gmail.com>
2023-11-23 03:51:32 -0500

    Update app.py

--- examples/simple_rank/app.py
+++ examples/simple_rank/app.py
@@ -1,1 +1,11 @@
+"""
+
+This should be a simple to boot, stand alone shiny app (not shinylive for now), that uses a local SqlLite database through SqlAlchemy (which can be later moved to be provided via the zs-state and zs-data mesh system), to run a simple rank choice widget demo
+
+from looking at this example, it should be easy to integrate a standalone, sqlite backed instance into a website
+
+and it should be VERY easy to integrate into another Shiny app, or Starlette based HTTP service
+
+"""
+
 app = App(app_ui, server)
