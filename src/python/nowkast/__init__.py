29a8acc748ba2d53de987ffc17cc7b57e1bc0621
wally@ <wally@>
2023-11-24 13:44:50 -0800

    sync z@d@h1: base classes integrated with downstream web repo

--- src/python/nowkast/__init__.py
+++ src/python/nowkast/__init__.py
@@ -1,45 +1,57 @@
 from dataclasses import dataclass
 
-from .base import Point
+# from . import Point
 
-@dataclass
-class Member: pass
+from .shiny_module import *
 
-@dataclass
-class Content(Point): pass
+from sqlmodel import Field, SQLModel, create_engine
 
-@dataclass
-class RankchoiceSelection: pass
+def dc(x):
+    return x
 
-chatid = str
 
-@dataclass
+
+@dc
+class Realm: pass
+    
+
+@dc
+class User: pass
+
+
+
+@dc
+class Content: 
+    creater: User = Field(
+    )
+
+
+
+@dc
 class Rankchoice:
-    owner: Member
-    admin: list[Member]
-    visible: bool
-    users: list[Member]
+    owner: User
+    
+    #admin: list[Member]
+    #visible: bool
+    #users: list[Member]
 
-    description: Point
+    desc: Content
 
     premise_gate_list: list[Content] 
     rank_choice_list: list[Content]
     
-    selections: list[RankchoiceSelection]
+    selections: list[Content]
 
-@dataclass
+
+@dc
 class Chat:
-    member_list: list[Member]
+    member_list: list[User]
     content_list: list[Content]
     
-    def get_content(self,since_datetime): pass
-    def push_content(self,push_datetime,content): pass
 
-@dataclass
-class NowkastStateProxy:
-    chat_dict: dict[chatid,Chat] = None
-
-    def obtain_chat(self,chat_id): pass
+@dc
+class State:
+    chat_list: list[Chat] = None
 
 
 
@@ -49,18 +61,5 @@ class NowkastStateProxy:
 
 
 
-
-@dataclass
-class Realm: pass
-
-@dataclass
-class User: pass
-
-@dataclass
-class Membership[dict[str,User]]: pass
-
-
-@dataclass
-class Chat: pass
 
 
--- src/python/nowkast/shiny_module.py
+++ src/python/nowkast/shiny_module.py
@@ -1,8 +1,28 @@
-@module.ui
-def rank_ui():
-    pass
+from shiny import module, Inputs, Outputs, Session, ui
 
-@module.server
-def rank_server(input, output, session):
-    pass
-    
+nkr = None
+
+class NowkastRuntime:
+
+    instance = None
+
+    @classmethod
+    def get_instance(cls):
+        
+        if cls.instance == None:
+            cls.instance = NowkastRuntime()
+        return cls.instance
+
+
+
+    @module.ui
+    def rank_ui(self):
+        pass
+
+    @module.server
+    def rank_server(self,input, output, session):        pass
+
+
+
+    def build_ui_widget(self,*args,**kwargs):
+        return ui.div("WIDGET")
